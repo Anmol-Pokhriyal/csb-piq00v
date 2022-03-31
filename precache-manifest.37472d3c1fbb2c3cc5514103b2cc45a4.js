938cb232852bf065c0c4c3a1976fb854b72d47e4
Code Sandbox <hello@codesandbox.io>
2022-03-31 14:13:37 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.37472d3c1fbb2c3cc5514103b2cc45a4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c0ca5a5adb79fd915f8ef01b030ea9c",
+    "url": "https://anmol-pokhriyal.github.io/csb-piq00v/index.html"
+  },
+  {
+    "revision": "e90b67013149e2df3d88",
+    "url": "https://anmol-pokhriyal.github.io/csb-piq00v/static/js/2.65f4fc52.chunk.js"
+  },
+  {
+    "revision": "328bf52b102868d502dd",
+    "url": "https://anmol-pokhriyal.github.io/csb-piq00v/static/js/main.2d274d45.chunk.js"
+  },
+  {
+    "revision": "aeea51105ac1fdc90923",
+    "url": "https://anmol-pokhriyal.github.io/csb-piq00v/static/js/runtime-main.74b5cc5c.js"
+  }
+]);
